0d973858d7d7a10922a15c37c038429a119800c5
rozariwang <hohsuann@gmail.com>
2023-06-13 20:05:47 +0200

    :)

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ with open('company_names.txt', 'r') as file:
     
 # Streamlit app code
 def main():
-    st.title("Tech Company Investment Recommendation")
+    st.title("US Company Investment Recommendation")
     
     # Dropdown selection box for companies
     selected_company = st.selectbox("What company do you want to invest in?", companies)
