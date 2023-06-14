8eba611befc66f3f77683705eb0e12056ad61b97
rozariwang <hohsuann@gmail.com>
2023-06-14 12:06:15 +0200

    :)

--- app.py
+++ app.py
@@ -1,7 +1,37 @@
 import streamlit as st
 import controller
 
+# Add custom CSS to align content in the middle
+st.markdown(
+    """
+    <style>
+    .stMarkdown, .stTitle {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
 st.title("🍎 Data Science Group 3 - NLP Financial Forcasting App")
+
+# Introduction section
+st.markdown(
+    """
+    ## Introduction
+    
+    Welcome to the NLP Financial Forecasting App! This app provides investment recommendations
+    for US companies based on natural language processing techniques and financial data analysis.
+    
+    Simply select a company from the dropdown list, click the Generate Recommendation button, and receive a 
+    recommendation tailored to your investment needs.
+    
+    Happy investing!
+    """
+)
 
 with open('company_names.txt', 'r') as file:
     companies= file.readlines() 
@@ -13,9 +43,6 @@ def main():
     # Dropdown selection box for companies
     selected_company = st.selectbox("What company do you want to invest in?", companies)
     
-    # Empty placeholder for apology message
-    apology_placeholder = st.empty()
-    
     # Submit button
     submit_button = st.button("Generate Recommendation")
     
@@ -23,17 +50,6 @@ def main():
         if selected_company:
         # Call controller's generate_recommendation() function to get recommendation
           response = controller.generate_recommendation(selected_company)
-          
-          if response is None:
-              apology_placeholder.subheader("Apologies!")
-              apology_placeholder.write(
-                    f"We cannot provide a recommendation for stock investment in company {selected_company}, "
-                    "as there is no data available for this company."
-               )
-          else:
-              # Display recommendation
-              st.subheader("Recommendation:")
-              st.write(response)
 
 if __name__ == '__main__':
     main()
