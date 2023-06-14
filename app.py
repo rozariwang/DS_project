a3c7d6ccd1413b5ad3f0742a615379d6da9d994a
rozariwang <hohsuann@gmail.com>
2023-06-14 11:46:36 +0200

    :)

--- app.py
+++ app.py
@@ -8,18 +8,29 @@ with open('company_names.txt', 'r') as file:
     
 # Streamlit app code
 def main():
-    st.title("US Company Investment Recommendation")
+    st.title("💸 US Company Investment Recommendation 🚀")
     
     # Dropdown selection box for companies
     selected_company = st.selectbox("What company do you want to invest in?", companies)
-
-    if selected_company:
+    
+    # Submit button
+    submit_button = st.button("Generate Recommendation")
+    
+    if submit_button:
+        if selected_company:
         # Call controller's generate_recommendation() function to get recommendation
-        response = controller.generate_recommendation(selected_company)
-        
-        # Display recommendation
-        st.subheader("Recommendation:")
-        st.write(response)
+          response = controller.generate_recommendation(selected_company)
+          
+          if response is None: 
+              st.subheader("Apologies!")
+              st.write(
+                  f"We cannot provide a recommendation for stock investment in company {selected_company}, "
+                  "as there is no data available for this company."
+              )
+          else:
+              # Display recommendation
+              st.subheader("Recommendation:")
+              st.write(response)
 
 if __name__ == '__main__':
     main()
