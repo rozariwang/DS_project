6822bc1df7507aa8e2c8e17f188580ceaf8c2aef
rozariwang <hohsuann@gmail.com>
2023-06-14 13:44:39 +0200

    :)

--- app.py
+++ app.py
@@ -69,7 +69,6 @@ def main():
                 search_history.append((selected_company, response))
    
     # Store updated search history in cache
-    get_search_history.cache_clear()
     get_search_history().extend(search_history)
     
     # Display search history section
