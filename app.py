5f9097aac37684df0a8a1be53788de1156de61c6
rozariwang <134708973+rozariwang@users.noreply.github.com>
2023-06-22 19:07:28 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import controller
 
 logo_image = "2.png"  # Replace with the actual path and filename
-st.image(logo_image, use_column_width=True)
+st.image(logo_image, width=150)
 
 # Add custom CSS to align content in the middle
 st.markdown(
