fabf5607cb42e7a1964597d0804b96f4ab820f7a
rozariwang <134708973+rozariwang@users.noreply.github.com>
2023-06-28 00:53:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -66,7 +66,9 @@ def main():
     selected_company = st.selectbox("##### What company do you want to invest in?", load_companies())
     st.markdown("<p style='font-size: 12px;'>You can either select a company from the list below or simply type in your selection.</p>", unsafe_allow_html=True)
     st.markdown("Regrettably, our app's predictions are limited to specific companies on Nasdaq and the New York Stock Exchange at the moment. We apologize for any inconvenience if the company you are interested in is not covered.")
-    
+
+    # Submit button
+    submit_button = st.button("Generate Recommendation")
     
     if submit_button:
         if selected_company:
