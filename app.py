3937fc07638f14ff04213daa1e4a21e84164c33f
rozariwang <134708973+rozariwang@users.noreply.github.com>
2023-06-28 00:25:57 +0200

    added user friendly notes.py

--- app.py
+++ app.py
@@ -65,7 +65,7 @@ def main():
     # Dropdown selection box for companies
     selected_company = st.selectbox("##### What company do you want to invest in?", load_companies())
     st.empty()
-    st.markdown("<p style='font-size: 12px; margin-top: -15px;'>You can either select a company from the list below or simply type in your selection.</p>", unsafe_allow_html=True)
+    st.markdown("<p style='font-size: 12px; margin-top: -15px;'>You can either select a company from the list or simply type in your selection.</p>", unsafe_allow_html=True)
 
     st.markdown("<p style='font-size: 12px;'>Regrettably, our app's predictions are limited to specific companies on Nasdaq and the New York Stock Exchange at the moment. We apologize for any inconvenience if the company you are interested in is not covered.</p>", unsafe_allow_html=True)
     
