814c9bcf43744441427cb2b8a6b927fc8ea67eaf
rozariwang <134708973+rozariwang@users.noreply.github.com>
2023-07-02 18:21:54 +0200

    Update temperature.py

--- controller.py
+++ controller.py
@@ -186,7 +186,7 @@ def generate_recommendation(company: str):
         max_tokens=1000,
         n=1,
         stop=None,
-        temperature=0.5
+        temperature=0.4
     )
     response = completion.choices[0].text # type: ignore
     return response
